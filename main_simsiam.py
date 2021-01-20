204fa2fe1dd95ad683d8b46b1d0f3bea2b8af2e3
yangyifan28 <yangyifan28@jd.com>
2021-01-21 01:17:37 +0800

    find_unused_parameters

--- main_simsiam.py
+++ main_simsiam.py
@@ -166,6 +166,7 @@ def main():
 
 def main_worker(gpu, ngpus_per_node, args):
     args.gpu = gpu
+    print('gpu : ', gpu)
 
     # suppress printing if not master
     if args.multiprocessing_distributed and args.gpu != 0:
@@ -205,7 +206,7 @@ def main_worker(gpu, ngpus_per_node, args):
             # ourselves based on the total number of GPUs we have
             args.batch_size = int(args.batch_size / ngpus_per_node)
             args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
-            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
             print('set')
         else:
             model.cuda()
